706c3d91a34a762601e2bc46c8434784b41081b7
Hassan Haider <zyrox762@gmail.com>
2026-09-20 22:57:56 -0700

    Initial Commit

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by !---------? on 20/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by !---------? on 20/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
